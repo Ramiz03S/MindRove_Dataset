7db1d00e6a30c56e8f855c1aac5fe562168ef5a8
Ramiz Samaien <ramizsamaien03@gmail.com>
2024-10-18 20:57:58 +0200

    implement the recording of multiple runs

--- dataset_recorder.py
+++ dataset_recorder.py
@@ -1,6 +1,7 @@
 from mindrove.board_shim import BoardShim, MindRoveInputParams, BoardIds, MindroveConfigMode
 import sys
 import time
+import random
 import threading
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
 from PySide6.QtGui import QPixmap
@@ -13,48 +14,35 @@ class TriggeredImageApp(QWidget):
         
     def initUI(self):
         self.setWindowTitle('MindRove Dataset')
-        self.setGeometry(100,100,400,400)
+        self.setGeometry(400,400,600,600)
         
+        self.cross = QLabel(self)
         self.label = QLabel(self)
         #self.label.setAlignment(QtCore.Qt.AlignCenter)
         
         layout = QVBoxLayout()
+        layout.addWidget(self.cross)
         layout.addWidget(self.label)
         self.setLayout(layout)
         
-        self.images = [QPixmap('right.png'),QPixmap('left.png')]
-        self.trigger_count = 0
+        self.images = [QPixmap('right.png'),QPixmap('left.png'),QPixmap('cross.png')]
     
-    def start(self):
-        thread = threading.Thread(target=self.run_process)
-        thread.start()
     
-    def run_process(self):
-        total_duration = 32
-        trigger_interval = 4
-        display_time = 4
-        
-        for i in range(int(total_duration/(trigger_interval+display_time))):
-            time.sleep(trigger_interval)
-            self.trigger_event()
-            time.sleep(display_time)
-            self.label.clear()
-        
-        self.close()
-    
-    def trigger_event(self):
-        self.label.setPixmap(self.images[self.trigger_count])
-        self.trigger_count +=1
-
+    def show_cross(self):
+        self.cross.setPixmap(self.images[2])
+    def clear_cross(self):
+        self.cross.clear()
+    def show_image(self, trigger_class):
+        self.label.setPixmap(self.images[trigger_class])
+    def clear_image(self):
+        self.label.clear()
 
 class arc_board:
-    def __init__(self, window_size, board_shim, board_id):
-        self.window_size = window_size
+    def __init__(self, board_shim, board_id):
         self.board_shim = board_shim
         self.eeg_channels = BoardShim.get_eeg_channels(board_id)
         self.trigger_channel = BoardShim.get_other_channels(19)
         self.sampling_rate = BoardShim.get_sampling_rate(board_id)
-        self.num_points = window_size * self.sampling_rate
         
     def prepare_session(self):
         self.board_shim.prepare_session()
@@ -65,33 +53,60 @@ class arc_board:
     def release_session(self):
         self.board_shim.release_session()
     
-    def record_window(self):
-        while True:
-            if self.board_shim.get_board_data_count() >= self.num_points:
-                data = self.board_shim.get_board_data(self.num_points)
-                eeg_data = data[self.eeg_channels]
-                triggers = data[19]
-                break
+    def record_window(self, window_size):
+        num_points = int(window_size * self.sampling_rate)
+        data = self.board_shim.get_board_data(num_points)
+        eeg_data = data[self.eeg_channels]
+        triggers = data[19]
         return [eeg_data,triggers]
     
-    def BEEP(self):
-        self.board_shim.config_board(MindroveConfigMode.BEEP)
-    def BOOP(self):
-        self.board_shim.config_board(MindroveConfigMode.BOOP)
+    def BEEP_BOOP(self, trigger_class):
+        if trigger_class == 0:
+            self.board_shim.config_board(MindroveConfigMode.BEEP)
+        if trigger_class == 1:
+            self.board_shim.config_board(MindroveConfigMode.BOOP)
 
-def run_arc():
+def init_arc():
     BoardShim.enable_dev_board_logger() # enable logger when developing to catch relevant logs 
-
     params = MindRoveInputParams() 
     board_id = BoardIds.MINDROVE_WIFI_BOARD
     board_shim = BoardShim(board_id, params)
-    window_size = 2 # seconds
+    return arc_board(board_shim, board_id)
+
+
+def run(display, arc_board, trigger_class):
+
+    time.sleep(4)
+    display.show_image(trigger_class)
+    arc_board.BEEP_BOOP(trigger_class)
+    time.sleep(4)
+    display.clear_image()
+
+def multiple_runs():
+    trigger_list = [0] * 12 + [1] * 12
+    random.shuffle(trigger_list)
+
+    arc_board = init_arc()
+    app = QApplication(sys.argv)
+    display = TriggeredImageApp()
+    arc_board.prepare_session()
     
-    arc_board(window_size, board_shim, board_id)
+    display.show()
+    display.show_cross()
+    
+    arc_board.start_streaming()
+    start_time = time.time()
+    
+    for i in trigger_list:
+        run(display,arc_board,i)
+    end_time = time.time()
+    arc_board.stop_streaming()
+    sys.exit(app.exec_())
+    
+    run_time = end_time - start_time
+    data, triggers = arc_board.record_window(run_time)
+    arc_board.release_session()
+    
     
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ex = TriggeredImageApp()
-    ex.show()
-    ex.start()
-    sys.exit(app.exec_())
+    multiple_runs()
